c76a957daec2bd542f78030a6aeb82a2f2801283
JustDoIt0910 <1396812324@qq.com>
2023-04-23 21:16:23 +0800

    update(mqtt_buffer)

--- mqtt_buffer.c
+++ mqtt_buffer.c
@@ -2,10 +2,14 @@
 // Created by zr on 23-4-9.
 //
 #include "mqtt_buffer.h"
+#include "mqtt_vec.h"
 #include "tlog.h"
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/uio.h>
 
 void tmq_buffer_init(tmq_buffer_t* buffer)
 {
@@ -40,26 +44,43 @@ static void buffer_chunk_realign(tmq_buffer_chunk_t* chunk)
 
 static tmq_buffer_chunk_t* find_free_chunk(tmq_buffer_t* buffer, size_t req_size)
 {
+    tmq_buffer_chunk_t* chunk = NULL;
     size_t free_list_idx = FREE_LIST_INDEX(req_size);
     for(; free_list_idx < 5; free_list_idx++)
     {
-        tmq_buffer_chunk_t* prev = NULL, *chunk = buffer->free_chunk_list[free_list_idx];
-        while(chunk && chunk->chunk_size < req_size)
-        {
-            prev = chunk;
-            chunk = chunk->next;
-        }
-        if(chunk)
-        {
-            if(!prev)
-                buffer->free_chunk_list[free_list_idx] = chunk->next;
-            else
-                prev->next = chunk->next;
-            chunk->next = NULL;
-            return chunk;
-        }
+        tmq_buffer_chunk_t** next = &buffer->free_chunk_list[free_list_idx];
+        while(*next && (*next)->chunk_size < req_size)
+            next = &(*next)->next;
+        if(!*next)
+            continue;
+        chunk = *next;
+        *next = (*next)->next;
+        chunk->next = NULL;
     }
-    return NULL;
+    return chunk;
+}
+
+static tmq_buffer_chunk_t* get_largest_free_chunk(tmq_buffer_t* buffer)
+{
+    int i = 4;
+    while(i > 0 && !buffer->free_chunk_list[i])
+        i--;
+    if(i < 0) return NULL;
+    tmq_buffer_chunk_t* largest = buffer->free_chunk_list[i];
+    if(i == 0)
+    {
+        buffer->free_chunk_list[0] = largest->next;
+        largest->next = NULL;
+    }
+    else
+    {
+        tmq_buffer_chunk_t** next = &buffer->free_chunk_list[i];
+        while((*next)->next)
+            next = &(*next)->next;
+        largest = *next;
+        *next = NULL;
+    }
+    return largest;
 }
 
 void tmq_buffer_append(tmq_buffer_t* buffer, const char* data, size_t size)
@@ -101,6 +122,44 @@ void tmq_buffer_append(tmq_buffer_t* buffer, const char* data, size_t size)
         new_chunk->write_idx += remain;
         chunk->next = new_chunk;
         buffer->last = new_chunk;
+    }
+    buffer->readable_bytes += size;
+}
+
+void tmq_buffer_prepend(tmq_buffer_t* buffer, const char* data, size_t size)
+{
+    tmq_buffer_chunk_t* chunk = buffer->first;
+    if(!chunk)
+    {
+        chunk = buffer_chunk_new(size);
+        memcpy(chunk->buf, data, size);
+        chunk->write_idx += size;
+        buffer->first = buffer->last = chunk;
+        return;
+    }
+    else if(chunk->read_idx >= size)
+    {
+        chunk->read_idx -= size;
+        memcpy(chunk->buf + chunk->read_idx, data, size);
+    }
+    else
+    {
+        size_t remain = size - chunk->read_idx;
+        tmq_buffer_chunk_t* new_chunk = find_free_chunk(buffer, remain);
+        if(!new_chunk)
+            new_chunk = buffer_chunk_new(remain);
+        if(!new_chunk) return;
+        size_t align = new_chunk->chunk_size - remain;
+        new_chunk->read_idx += align;
+        memcpy(new_chunk->buf + new_chunk->read_idx, data, remain);
+        new_chunk->write_idx += new_chunk->read_idx + remain;
+        data += remain;
+        size -= remain;
+        if(size)
+        {
+            chunk->read_idx -= size;
+            memcpy(chunk->buf + chunk->read_idx, data, size);
+        }
     }
     buffer->readable_bytes += size;
 }
@@ -197,4 +256,74 @@ void tmq_buffer_remove(tmq_buffer_t* buffer, size_t size)
     }
     if(chunk && size)
         chunk->read_idx += size;
+    buffer->readable_bytes -= size;
+}
+
+ssize_t tmq_buffer_read_fd(tmq_buffer_t* buffer, tmq_socket_t fd, size_t max)
+{
+    if(!buffer) return 0;
+    int fd_readable = 0;
+    if(ioctl(fd, FIONREAD, &fd_readable) < 0)
+    {
+        tlog_error("ioctl() error %d: %s", errno, strerror(errno));
+        return 0;
+    }
+    size_t size;
+    if(!max)
+        size = min(fd_readable, FD_MAX_READ_BYTES);
+    else
+        size = min(fd_readable, min(max, FD_MAX_READ_BYTES));
+    int iovec_cnt = 0;
+    size_t space_aval = 0;
+    struct iovec vecs[MAX_IOVEC_NUM];
+    tmq_buffer_chunk_t* chunk = buffer->last;
+    if(chunk && CHUNK_WRITEABLE(chunk) > 0)
+    {
+        vecs[0].iov_base = chunk->buf + chunk->write_idx;
+        vecs[0].iov_len = CHUNK_WRITEABLE(chunk);
+        iovec_cnt++;
+        space_aval += CHUNK_WRITEABLE(chunk);
+    }
+    for(int i = iovec_cnt; space_aval < size && i < MAX_IOVEC_NUM; i++)
+    {
+        chunk = find_free_chunk(buffer, size - space_aval);
+        if(!chunk && i < MAX_IOVEC_NUM - 1)
+            chunk = get_largest_free_chunk(buffer);
+        if(!chunk)
+            chunk = buffer_chunk_new(size - space_aval);
+        assert(chunk && chunk->chunk_size > 0);
+        buffer->last->next = chunk;
+        buffer->last = chunk;
+        space_aval += chunk->chunk_size;
+        vecs[i].iov_base = chunk->buf;
+        vecs[i].iov_len = chunk->chunk_size;
+        iovec_cnt++;
+    }
+    ssize_t n = readv(fd, vecs, iovec_cnt);
+    if(n > 0)
+        buffer->readable_bytes += n;
+    return n;
+}
+
+ssize_t tmq_buffer_write_fd(tmq_buffer_t* buffer, tmq_socket_t fd)
+{
+    if(!buffer) return 0;
+    tmq_vec(struct iovec) vecs = tmq_vec_make(struct iovec);
+    tmq_buffer_chunk_t* chunk = buffer->first;
+    int iovec_cnt = 0;
+    while (chunk)
+    {
+        struct iovec iov = {
+                .iov_base = chunk->buf + chunk->read_idx,
+                .iov_len = CHUNK_DATA_LEN(chunk)
+        };
+        tmq_vec_push_back(vecs, iov);
+        chunk = chunk->next;
+    }
+    ssize_t n = writev(fd, tmq_vec_begin(vecs), iovec_cnt);
+    tmq_vec_free(vecs);
+    if(n < 0)
+        return n;
+    tmq_buffer_remove(buffer, n);
+    return n;
 }
